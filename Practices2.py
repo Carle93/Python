dcd03b9f754f639deecacbd88ac09e003ab23899
carlexi niño <isley7777@gmail.com>
2023-04-18 16:46:14 -0500

    segunda practica

---
+++ Practices2.py
@@ -0,0 +1,19 @@
+#entradas con retorno de un str
+num = input (" ingresa un numero: ")
+print (type( num))
+
+#entradas con retorno de dato int
+num_1 = int(input ("ingresa un numero: "))
+print (type( num_1))
+
+#operadores Logicos and, or , not en python 
+num_2 = 5 > 4 and 6 < 8
+print (num_2) 
+
+#operador or ( uno de las expresiones es verdadera)
+num_3 = 5 > 6 or 6 > 4
+print (num_3)
+
+#operador not  ( en este caso la expresion es verdadera y nos )
+num_4 = not 6 <  5
+print (num_4)
